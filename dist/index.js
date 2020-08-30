ce51a374de4d4fb89d91eee27481d7e4c14d9ba3
Quang Che <quang.n.che@gmail.com>
2020-08-30 13:35:34 -0700

    dist folder updates

--- dist/index.js
+++ dist/index.js
@@ -8,21 +8,32 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@mikro-orm/core");
-const constants_1 = require("./constants");
-const Post_1 = require("./entities/Post");
+const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const express_1 = __importDefault(require("express"));
+const apollo_server_express_1 = require("apollo-server-express");
+const type_graphql_1 = require("type-graphql");
+const hello_1 = require("./resolvers/hello");
+const post_1 = require("./resolvers/post");
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
-    const orm = yield core_1.MikroORM.init({
-        entities: [Post_1.Post],
-        dbName: 'readdit',
-        type: 'postgresql',
-        debug: !constants_1.__prod__,
+    const orm = yield core_1.MikroORM.init(mikro_orm_config_1.default);
+    yield orm.getMigrator().up();
+    const app = express_1.default();
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        schema: yield type_graphql_1.buildSchema({
+            resolvers: [hello_1.HelloResolver, post_1.PostResolver],
+            validate: false
+        }),
+        context: () => ({ em: orm.em })
     });
-    const post = orm.em.create(Post_1.Post, { title: 'My Firs Post' });
-    yield orm.em.persistAndFlush(post);
-    console.log('-----------sql 2----------');
-    yield orm.em.nativeInsert(Post_1.Post, { title: 'My Second Post' });
+    apolloServer.applyMiddleware({ app });
+    app.listen(4000, () => {
+        console.log('Server started on localhost:4000');
+    });
 });
 main().catch(err => console.log(err));
 //# sourceMappingURL=index.js.map
